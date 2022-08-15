333d87cb2738cb477cc5c3c98eea70f9e4aaee7a
Rafal Jankowski <raalsky@gmail.com>
2022-08-15 14:20:37 +0200

    Added base and development requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+base_requirements = []
+
+develop_requirements = [
+    "pre-commit",
+    "pytest",
+]
+
 setup(
     name="neptune-f1",
-    version="0.0.1",
+    version="0.1.0",
     packages=["neptune_f1"],
     url="https://github.com/Raalsky/neptune-f1",
     license="MIT",
     author="raalsky",
     author_email="raalsky@gmail.com",
     description="EA F1 2021 telemetry integration with Neptune.ai",
+    install_requires=base_requirements,
+    extras_require={"dev": develop_requirements},
 )
