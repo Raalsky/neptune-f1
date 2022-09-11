dccd078ed3849a52f3ba9743596ee311da13241d
Rafal Jankowski <raalsky@gmail.com>
2022-09-11 15:11:33 +0200

    Added tests workflow

--- tests/integration/test_parser.py
+++
@@ -1,7 +0,0 @@
-from f1_2021.packets import Parser
-
-
-class TestParser:
-    def test_simple(self):
-        packets = list(Parser.from_file("data/sample_packets/03_event.bin"))
-        assert type(packets[0]).__name__ == "PacketEventData"
