d6cc3936829c65b1757c0a0d385315ce3a3c77d6
Rafal Jankowski <raalsky@gmail.com>
2022-10-02 20:06:07 +0200

    Added tests for lap data

--- tests/unit/test_packet_lap_data.py
+++ tests/unit/test_packet_lap_data.py
@@ -6,6 +6,171 @@ from neptune_f1.packets.codemasters_f12021.packet_header import PacketHeader
 from neptune_f1.packets.codemasters_f12021.packet_lap_data import LapData, PacketLapData
 
 
+def single():
+    return LapData(
+        m_last_lap_time_in_ms=83210,
+        m_current_lap_time_in_ms=80925,
+        m_sector1_time_in_ms=21481,
+        m_sector2_time_in_ms=35023,
+        m_lap_distance=4023.0,
+        m_total_distance=39421.5,
+        m_safety_car_delta=0.0,
+        m_car_position=2,
+        m_current_lap_num=18,
+        m_pit_status=0,
+        m_num_pit_stops=2,
+        m_sector=2,
+        m_current_lap_invalid=0,
+        m_penalties=0,
+        m_warnings=1,
+        m_num_unserved_drive_through_pens=0,
+        m_num_unserved_stop_go_pens=0,
+        m_grid_position=5,
+        m_driver_status=2,
+        m_result_status=2,
+        m_pit_lane_timer_active=0,
+        m_pit_lane_time_in_lane_in_ms=0,
+        m_pit_stop_timer_in_ms=0,
+        m_pit_stop_should_serve_pen=0,
+    )
+
+
+def single__dict_representation():
+    return {
+        "m_last_lap_time_in_ms": 83210,
+        "m_current_lap_time_in_ms": 80925,
+        "m_sector1_time_in_ms": 21481,
+        "m_sector2_time_in_ms": 35023,
+        "m_lap_distance": 4023.0,
+        "m_total_distance": 39421.5,
+        "m_safety_car_delta": 0.0,
+        "m_car_position": 2,
+        "m_current_lap_num": 18,
+        "m_pit_status": 0,
+        "m_num_pit_stops": 2,
+        "m_sector": 2,
+        "m_current_lap_invalid": 0,
+        "m_penalties": 0,
+        "m_warnings": 1,
+        "m_num_unserved_drive_through_pens": 0,
+        "m_num_unserved_stop_go_pens": 0,
+        "m_grid_position": 5,
+        "m_driver_status": 2,
+        "m_result_status": 2,
+        "m_pit_lane_timer_active": 0,
+        "m_pit_lane_time_in_lane_in_ms": 0,
+        "m_pit_stop_timer_in_ms": 0,
+        "m_pit_stop_should_serve_pen": 0,
+    }
+
+
+def single__json_representation():
+    return """{
+      "m_car_position": 2,
+      "m_current_lap_invalid": 0,
+      "m_current_lap_num": 18,
+      "m_current_lap_time_in_ms": 80925,
+      "m_driver_status": 2,
+      "m_grid_position": 5,
+      "m_lap_distance": 4023.0,
+      "m_last_lap_time_in_ms": 83210,
+      "m_num_pit_stops": 2,
+      "m_num_unserved_drive_through_pens": 0,
+      "m_num_unserved_stop_go_pens": 0,
+      "m_penalties": 0,
+      "m_pit_lane_time_in_lane_in_ms": 0,
+      "m_pit_lane_timer_active": 0,
+      "m_pit_status": 0,
+      "m_pit_stop_should_serve_pen": 0,
+      "m_pit_stop_timer_in_ms": 0,
+      "m_result_status": 2,
+      "m_safety_car_delta": 0.0,
+      "m_sector": 2,
+      "m_sector1_time_in_ms": 21481,
+      "m_sector2_time_in_ms": 35023,
+      "m_total_distance": 39421.5,
+      "m_warnings": 1
+    }"""
+
+
+def single__binary_representation():
+    return (
+        b"\x0a\x45\x01\x00\x1d\x3c\x01\x00\xe9\x53\xcf\x88\x00\x70\x7b\x45\x80\xfd\x19\x47\x00\x00\x00\x00\x02\x12\x00"
+        b"\x02\x02\x00\x00\x01\x00\x00\x05\x02\x02\x00\x00\x00\x00\x00\x00"
+    )
+
+
+@pytest.fixture
+def packet():
+    return PacketLapData(
+        m_header=PacketHeader(
+            m_packet_format=2021,
+            m_game_major_version=1,
+            m_game_minor_version=23,
+            m_packet_version=25,
+            m_packet_id=2,
+            m_session_uid=2501,
+            m_session_time=25.01,
+            m_frame_identifier=123,
+            m_player_car_index=6,
+            m_secondary_player_car_index=255,
+        ),
+        m_lap_data=(LapData * 22)(*[single() for _ in range(22)]),
+    )
+
+
+@pytest.fixture
+def dict_representation():
+    return {
+        "m_header": {
+            "m_packet_format": 2021,
+            "m_game_major_version": 1,
+            "m_game_minor_version": 23,
+            "m_packet_version": 25,
+            "m_packet_id": 2,
+            "m_session_uid": 2501,
+            "m_session_time": 25.01,
+            "m_frame_identifier": 123,
+            "m_player_car_index": 6,
+            "m_secondary_player_car_index": 255,
+        },
+        "m_lap_data": [single__dict_representation() for _ in range(22)],
+    }
+
+
+@pytest.fixture
+def json_representation():
+    entries = ",\n    ".join(single__json_representation() for _ in range(22))
+
+    return (
+        "{\n"
+        '  "m_header": {\n'
+        '    "m_frame_identifier": 123,\n'
+        '    "m_game_major_version": 1,\n'
+        '    "m_game_minor_version": 23,\n'
+        '    "m_packet_format": 2021,\n'
+        '    "m_packet_id": 2,\n'
+        '    "m_packet_version": 25,\n'
+        '    "m_player_car_index": 6,\n'
+        '    "m_secondary_player_car_index": 255,\n'
+        '    "m_session_time": 25.01,\n'
+        '    "m_session_uid": 2501\n'
+        "  },\n"
+        '  "m_lap_data": [\n    '
+        f"{entries}\n"
+        "  ]\n"
+        "}"
+    )
+
+
+@pytest.fixture
+def binary_representation():
+    return (
+        b"\xe5\x07\x01\x17\x19\x02\xc5\x09\x00\x00\x00\x00\x00\x00\x7b\x14\xc8\x41\x7b\x00\x00\x00\x06\xff"
+        + single__binary_representation() * 22
+    )
+
+
 @pytest.mark.parametrize(
     "type_name, type_class",
     [
@@ -83,3 +248,28 @@ def test_packet_lap_data__field__types(type_name, type_class):
     packet_type = PacketLapData()
 
     assert (type_name, type_class) in packet_type._fields_
+
+
+def test_packet_car_lap_data__to_dict(packet, dict_representation):
+    assert packet.to_dict() == dict_representation
+
+
+def test_packet_car_lap_data__get_value(packet, dict_representation):
+    for field, value in dict_representation.items():
+        assert packet.get_value(field=field) == value
+
+
+def test_packet_car_lap_data__to_json(packet, json_representation):
+    print(packet.to_json())
+    assert packet.to_json() == json_representation
+
+
+def test_packet_car_lap_data__to_binary(packet, binary_representation):
+    print("".join(f"\\x{x:02x}" for x in packet.pack()))
+    assert packet.pack() == binary_representation
+
+
+def test_packet_car_lap_data__from_binary(binary_representation, dict_representation):
+    packet = PacketLapData.unpack(binary_representation)
+
+    assert packet.to_dict() == dict_representation
