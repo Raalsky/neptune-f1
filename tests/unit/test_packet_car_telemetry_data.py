d20eb0116f3f7c1e6aa23f97cb24eb22c5192b0c
Rafal Jankowski <raalsky@gmail.com>
2022-10-02 18:11:37 +0200

    Added tests for car telemetry data

--- tests/unit/test_packet_car_telemetry_data.py
+++ tests/unit/test_packet_car_telemetry_data.py
@@ -6,6 +6,183 @@ from neptune_f1.packets.codemasters_f12021.packet_car_telemetry_data import CarT
 from neptune_f1.packets.codemasters_f12021.packet_header import PacketHeader
 
 
+def single():
+    return CarTelemetryData(
+        m_speed=254,
+        m_throttle=1.0,
+        m_steer=-0.5,
+        m_brake=0.0,
+        m_clutch=0,
+        m_gear=7,
+        m_engine_rpm=12345,
+        m_drs=1,
+        m_rev_lights_percent=70,
+        m_rev_lights_bit_value=0,
+        m_brakes_temperature=(ctypes.c_uint16 * 4)(450, 460, 470, 480),
+        m_tyres_surface_temperature=(ctypes.c_uint8 * 4)(50, 60, 70, 80),
+        m_tyres_inner_temperature=(ctypes.c_uint8 * 4)(45, 55, 65, 75),
+        m_engine_temperature=90,
+        m_tyres_pressure=(ctypes.c_float * 4)(0.5, 0.5, 1.0, 1.0),
+        m_surface_type=(ctypes.c_uint8 * 4)(0, 1, 2, 3),
+    )
+
+
+def single__dict_representation():
+    return {
+        "m_speed": 254,
+        "m_throttle": 1.0,
+        "m_steer": -0.5,
+        "m_brake": 0.0,
+        "m_clutch": 0,
+        "m_gear": 7,
+        "m_engine_rpm": 12345,
+        "m_drs": 1,
+        "m_rev_lights_percent": 70,
+        "m_rev_lights_bit_value": 0,
+        "m_brakes_temperature": [450, 460, 470, 480],
+        "m_tyres_surface_temperature": [50, 60, 70, 80],
+        "m_tyres_inner_temperature": [45, 55, 65, 75],
+        "m_engine_temperature": 90,
+        "m_tyres_pressure": [0.5, 0.5, 1.0, 1.0],
+        "m_surface_type": [0, 1, 2, 3],
+    }
+
+
+def single__json_representation():
+    return """{
+      "m_brake": 0.0,
+      "m_brakes_temperature": [
+        450,
+        460,
+        470,
+        480
+      ],
+      "m_clutch": 0,
+      "m_drs": 1,
+      "m_engine_rpm": 12345,
+      "m_engine_temperature": 90,
+      "m_gear": 7,
+      "m_rev_lights_bit_value": 0,
+      "m_rev_lights_percent": 70,
+      "m_speed": 254,
+      "m_steer": -0.5,
+      "m_surface_type": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "m_throttle": 1.0,
+      "m_tyres_inner_temperature": [
+        45,
+        55,
+        65,
+        75
+      ],
+      "m_tyres_pressure": [
+        0.5,
+        0.5,
+        1.0,
+        1.0
+      ],
+      "m_tyres_surface_temperature": [
+        50,
+        60,
+        70,
+        80
+      ]
+    }"""
+
+
+def single__binary_representation():
+    return (
+        b"\xfe\x00\x00\x00\x80\x3f\x00\x00\x00\xbf\x00\x00\x00\x00\x00\x07\x39\x30\x01\x46\x00\x00\xc2\x01\xcc\x01\xd6"
+        b"\x01\xe0\x01\x32\x3c\x46\x50\x2d\x37\x41\x4b\x5a\x00\x00\x00\x00\x3f\x00\x00\x00\x3f\x00\x00\x80\x3f\x00\x00"
+        b"\x80\x3f\x00\x01\x02\x03"
+    )
+
+
+@pytest.fixture
+def packet():
+    return PacketCarTelemetryData(
+        m_header=PacketHeader(
+            m_packet_format=2021,
+            m_game_major_version=1,
+            m_game_minor_version=23,
+            m_packet_version=25,
+            m_packet_id=6,
+            m_session_uid=2501,
+            m_session_time=25.01,
+            m_frame_identifier=123,
+            m_player_car_index=6,
+            m_secondary_player_car_index=255,
+        ),
+        m_car_telemetry_data=(CarTelemetryData * 22)(*[single() for _ in range(22)]),
+        m_mfd_panel_index=255,
+        m_mfd_panel_index_secondary_player=255,
+        m_suggested_gear=7,
+    )
+
+
+@pytest.fixture
+def dict_representation():
+    return {
+        "m_header": {
+            "m_packet_format": 2021,
+            "m_game_major_version": 1,
+            "m_game_minor_version": 23,
+            "m_packet_version": 25,
+            "m_packet_id": 6,
+            "m_session_uid": 2501,
+            "m_session_time": 25.01,
+            "m_frame_identifier": 123,
+            "m_player_car_index": 6,
+            "m_secondary_player_car_index": 255,
+        },
+        "m_car_telemetry_data": [single__dict_representation() for _ in range(22)],
+        "m_mfd_panel_index": 255,
+        "m_mfd_panel_index_secondary_player": 255,
+        "m_suggested_gear": 7,
+    }
+
+
+@pytest.fixture
+def json_representation():
+    entries = ",\n    ".join(single__json_representation() for _ in range(22))
+
+    return (
+        "{\n"
+        '  "m_car_telemetry_data": [\n    '
+        f"{entries}\n"
+        "  ],\n"
+        '  "m_header": {\n'
+        '    "m_frame_identifier": 123,\n'
+        '    "m_game_major_version": 1,\n'
+        '    "m_game_minor_version": 23,\n'
+        '    "m_packet_format": 2021,\n'
+        '    "m_packet_id": 6,\n'
+        '    "m_packet_version": 25,\n'
+        '    "m_player_car_index": 6,\n'
+        '    "m_secondary_player_car_index": 255,\n'
+        '    "m_session_time": 25.01,\n'
+        '    "m_session_uid": 2501\n'
+        "  },\n"
+        '  "m_mfd_panel_index": 255,\n'
+        '  "m_mfd_panel_index_secondary_player": 255,\n'
+        '  "m_suggested_gear": 7\n'
+        "}"
+    )
+
+
+@pytest.fixture
+def binary_representation():
+    return (
+        b"\xe5\x07\x01\x17\x19\x06\xc5\x09\x00\x00\x00\x00\x00\x00\x7b\x14\xc8\x41\x7b\x00\x00\x00\x06\xff"
+        + single__binary_representation() * 22
+        + b"\xff\xff\x07"
+    )
+
+
 @pytest.mark.parametrize(
     "type_name, type_class",
     [
@@ -71,3 +248,26 @@ def test_packet_car_telemetry_data__field__types(type_name, type_class):
     packet_type = PacketCarTelemetryData()
 
     assert (type_name, type_class) in packet_type._fields_
+
+
+def test_packet_car_telemetry_data__to_dict(packet, dict_representation):
+    assert packet.to_dict() == dict_representation
+
+
+def test_packet_car_telemetry_data__get_value(packet, dict_representation):
+    for field, value in dict_representation.items():
+        assert packet.get_value(field=field) == value
+
+
+def test_packet_car_telemetry_data__to_json(packet, json_representation):
+    assert packet.to_json() == json_representation
+
+
+def test_packet_car_telemetry_data__to_binary(packet, binary_representation):
+    assert packet.pack() == binary_representation
+
+
+def test_packet_car_telemetry_data__from_binary(binary_representation, dict_representation):
+    packet = PacketCarTelemetryData.unpack(binary_representation)
+
+    assert packet.to_dict() == dict_representation
